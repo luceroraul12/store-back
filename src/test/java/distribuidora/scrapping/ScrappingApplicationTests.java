aa2e1b6969323b56b46d93d0f5daf4e7d0d23331
Lucero Raul <luceroraul12@gmail.com>
2022-09-28 14:20:49 -0300

    commit inicial

---
+++ src/test/java/distribuidora/scrapping/ScrappingApplicationTests.java
@@ -0,0 +1,13 @@
+package distribuidora.scrapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
