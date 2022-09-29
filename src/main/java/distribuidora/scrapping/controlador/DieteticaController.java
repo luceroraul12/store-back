a78c01bbf749ac07caebefc2f3a6279ca8ccbb89
Lucero Raul <luceroraul12@gmail.com>
2022-09-28 22:12:24 -0300

    fix problema del cors

--- src/main/java/distribuidora/scrapping/controlador/DieteticaController.java
+++ src/main/java/distribuidora/scrapping/controlador/DieteticaController.java
@@ -7,6 +7,7 @@ import distribuidora.scrapping.servicios.LaGranjaDelCentroServicio;
 import distribuidora.scrapping.servicios.MelarSeleniumServicio;
 import distribuidora.scrapping.servicios.SudamerikServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("scrapping")
+@RequestMapping(value = "scrapping")
 public class DieteticaController {
 
     @Autowired
