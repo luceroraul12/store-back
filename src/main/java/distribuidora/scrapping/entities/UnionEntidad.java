ebddd3328330f3432cdcae9016210bf92dba0145
Lucero Raul <luceroraul12@gmail.com>
2022-10-04 12:14:28 -0300

    agrego attr distribuidora para saber de cual es

--- src/main/java/distribuidora/scrapping/entities/UnionEntidad.java
+++ src/main/java/distribuidora/scrapping/entities/UnionEntidad.java
@@ -1,15 +1,19 @@
 package distribuidora.scrapping.entities;
 
+import distribuidora.scrapping.enums.Distribuidora;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Data
 @Document(collection = "union")
 public class UnionEntidad<Entidad> {
     @Id
     private String id;
     private List<Entidad> datos;
     private LocalDate fechaScrap;
+    private Distribuidora distribuidora;
 }
