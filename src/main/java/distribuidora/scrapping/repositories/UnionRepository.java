389bdc16002b418e255a3c9391f0b568b273a34c
Lucero Raul <luceroraul12@gmail.com>
2022-10-04 12:15:14 -0300

    creo un metodo JPQL para obtener los productos por medio de la enumeracion que identifica a al distribuidora

--- src/main/java/distribuidora/scrapping/repositories/UnionRepository.java
+++ src/main/java/distribuidora/scrapping/repositories/UnionRepository.java
@@ -1,9 +1,18 @@
 package distribuidora.scrapping.repositories;
 
 import distribuidora.scrapping.entities.UnionEntidad;
+import distribuidora.scrapping.enums.Distribuidora;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UnionRepository extends MongoRepository<UnionEntidad,String> {
+public interface UnionRepository<Entidad> extends MongoRepository<UnionEntidad<Entidad>,String> {
+
+    @Query("Select u from UnionEntidad where u.distribuidora = :distribuidora")
+    UnionEntidad<Entidad> obtenerProductos(@Param("distribuidora") Distribuidora distribuidora);
+
 }
