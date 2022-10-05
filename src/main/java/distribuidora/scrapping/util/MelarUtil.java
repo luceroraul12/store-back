f1d311f40ea852691d1657b733c751c0692859c4
Lucero Raul <luceroraul12@gmail.com>
2022-10-05 19:02:13 -0300

    add melar util para convertir a producto

---
+++ src/main/java/distribuidora/scrapping/util/MelarUtil.java
@@ -0,0 +1,57 @@
+package distribuidora.scrapping.util;
+
+import distribuidora.scrapping.entities.MelarEntidad;
+import distribuidora.scrapping.entities.Producto;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Component
+public class MelarUtil extends ProductoUtil<MelarEntidad>{
+    @Override
+    Collection<Producto> convertirProductoyDevolverlo(MelarEntidad productoSinConvertir) {
+        Collection<Producto> productosCreados = new ArrayList<>();
+
+        /*
+        Quiero tener un descripcion en el siguiente orden
+        1.producto
+        2.origen
+        3.cantidad
+        4.precio
+         */
+
+        String descripcionFraccion = String.format(
+                "%s - %s X %s%s",
+                productoSinConvertir.getProducto(),
+                productoSinConvertir.getOrigen(),
+                productoSinConvertir.getFraccion(),
+                productoSinConvertir.getMedida()
+        );
+
+        String descripcionGranel = String.format(
+                "%s - %s X %s%s",
+                productoSinConvertir.getProducto(),
+                productoSinConvertir.getOrigen(),
+                productoSinConvertir.getGranel(),
+                productoSinConvertir.getMedida()
+        );
+
+        productosCreados.add(
+                Producto
+                        .builder()
+                        .descripcion(descripcionFraccion)
+                        .precioPorCantidadEspecifica(productoSinConvertir.getPrecioFraccionado())
+                        .build()
+        );
+        productosCreados.add(
+                Producto
+                        .builder()
+                        .descripcion(descripcionGranel)
+                        .precioPorCantidadEspecifica(productoSinConvertir.getPrecioGranel())
+                        .build()
+        );
+
+        return productosCreados;
+    }
+}
