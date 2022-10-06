f46a825616c5c26ca584a5527cd28e086699e3c9
Lucero Raul <luceroraul12@gmail.com>
2022-10-06 17:23:49 -0300

    add termino de busqueda

--- src/main/java/distribuidora/scrapping/controller/DieteticaController.java
+++ src/main/java/distribuidora/scrapping/controller/DieteticaController.java
@@ -1,21 +1,15 @@
 package distribuidora.scrapping.controller;
 
-import distribuidora.scrapping.entities.LaGranjaDelCentroEntidad;
-import distribuidora.scrapping.entities.MelarEntidad;
 import distribuidora.scrapping.entities.Producto;
-import distribuidora.scrapping.entities.SudamerikEntidad;
-import distribuidora.scrapping.services.LaGranjaDelCentroServicio;
-import distribuidora.scrapping.services.MelarSeleniumServicio;
 import distribuidora.scrapping.services.RecolectorDeProductosServicio;
-import distribuidora.scrapping.services.SudamerikServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "scrapping")
@@ -25,8 +19,8 @@ public class DieteticaController {
     RecolectorDeProductosServicio recolectorDeProductosServicio;
 
     @GetMapping("productos")
-    public Collection<Producto> obtenerTodosLosProductos() throws IOException {
-        return recolectorDeProductosServicio.obtenerTodosLosProductos();
+    public Collection<Producto> obtenerTodosLosProductos(@RequestParam(name = "busqueda") String busqueda) throws IOException {
+        return recolectorDeProductosServicio.obtenerTodosLosProductos(busqueda);
     }
 
 }
