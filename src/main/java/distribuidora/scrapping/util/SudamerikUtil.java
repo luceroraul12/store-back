7e059725d048eeda8d4682d458b6e7bff7bf0e34
Lucero Raul <luceroraul12@gmail.com>
2022-10-05 22:13:09 -0300

    conversion implementada

--- src/main/java/distribuidora/scrapping/util/SudamerikUtil.java
+++ src/main/java/distribuidora/scrapping/util/SudamerikUtil.java
@@ -2,12 +2,48 @@ package distribuidora.scrapping.util;
 
 import distribuidora.scrapping.entities.Producto;
 import distribuidora.scrapping.entities.SudamerikEntidad;
+import distribuidora.scrapping.enums.Distribuidora;
+import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
+@Component
 public class SudamerikUtil extends ProductoUtil<SudamerikEntidad> {
     @Override
     Collection<Producto> convertirProductoyDevolverlo(SudamerikEntidad productoSinConvertir) {
-        return null;
+        String cantidadEspecifica = productoSinConvertir.getCantidadEspecifca();
+        String nombreProducto = productoSinConvertir.getNombreProducto();
+
+        String[] arregloCantidad = cantidadEspecifica.split(" ");
+        String descripcion = "";
+
+        boolean estaContenido = false;
+
+        for (String parte : arregloCantidad){
+            if (nombreProducto.contains(parte)){
+                descripcion = nombreProducto;
+                estaContenido = true;
+                break;
+            }
+        }
+        if (!estaContenido){
+            /*
+            Para este caso, en caso de no tenerlo, simplemente concateno ambos strings y nada mas.
+             */
+            descripcion = String.format(
+                    "%s X %s",
+                    nombreProducto,
+                    cantidadEspecifica
+            );
+        }
+
+        return Collections.singleton(Producto
+                .builder()
+                .descripcion(descripcion)
+                .precioPorCantidadEspecifica(productoSinConvertir.getPrecio())
+                .distribuidora(Distribuidora.SUDAMERIK)
+                .build());
     }
 }
