b96e68243d2a411555500c830608f7f16bce16aa
Lucero Raul <luceroraul12@gmail.com>
2022-10-05 22:24:17 -0300

    implementacion de conversion

---
+++ src/main/java/distribuidora/scrapping/util/LaGranjaDelCentroUtil.java
@@ -0,0 +1,21 @@
+package distribuidora.scrapping.util;
+
+import distribuidora.scrapping.entities.LaGranjaDelCentroEntidad;
+import distribuidora.scrapping.entities.Producto;
+import distribuidora.scrapping.enums.Distribuidora;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Component
+public class LaGranjaDelCentroUtil extends ProductoUtil<LaGranjaDelCentroEntidad>{
+    @Override
+    Collection<Producto> convertirProductoyDevolverlo(LaGranjaDelCentroEntidad productoSinConvertir) {
+        return Collections.singleton(Producto.builder()
+                .descripcion(productoSinConvertir.getNombreProducto())
+                .precioPorCantidadEspecifica(productoSinConvertir.getPrecio())
+                .distribuidora(Distribuidora.LA_GRANJA_DEL_CENTRO)
+                .build());
+    }
+}
