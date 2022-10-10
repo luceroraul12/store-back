7b4a34ad115bd9d96e55fc2100b37ac5f59bb23c
Lucero Raul <luceroraul12@gmail.com>
2022-10-10 12:07:58 -0300

    adaptacion para poder desplegarlo en apache tomcat sin problemas

--- src/main/java/distribuidora/scrapping/ScrappingApplication.java
+++ src/main/java/distribuidora/scrapping/ScrappingApplication.java
@@ -2,10 +2,17 @@ package distribuidora.scrapping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-public class ScrappingApplication {
+public class ScrappingApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ScrappingApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScrappingApplication.class, args);
