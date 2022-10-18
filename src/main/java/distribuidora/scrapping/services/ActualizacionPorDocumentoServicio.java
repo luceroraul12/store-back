ec1cb3de86d93b83463cf2adda1211832420c7c7
Lucero Raul <luceroraul12@gmail.com>
2022-10-18 16:39:00 -0300

    add servicio selector del servicio en funcion de la respuesta del frontend por medio de la enumeracion

---
+++ src/main/java/distribuidora/scrapping/services/ActualizacionPorDocumentoServicio.java
@@ -0,0 +1,40 @@
+package distribuidora.scrapping.services;
+
+import distribuidora.scrapping.entities.PeticionFrontEndDocumento;
+import distribuidora.scrapping.enums.Distribuidora;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Collection;
+
+@Service
+public class ActualizacionPorDocumentoServicio {
+    @Autowired
+    private IndiasServicio indiasServicio;
+    @Autowired
+    private FacundoServicio facundoServicio;
+
+    public void recibirDocumento(PeticionFrontEndDocumento documento) throws IOException {
+        Distribuidora distribuidora = documento.getDistribuidora();
+
+        System.out.println(distribuidora);
+//        indiasServicio.obtenerProductos(documento.getExcels()).forEach(System.out::println);
+//        facundoServicio.obtenerProductos(documento.getExcels()).forEach(System.out::println);
+        identificarDistribuidorayEjecutar(documento).forEach(System.out::println);
+    }
+
+
+    private Collection<?> identificarDistribuidorayEjecutar(PeticionFrontEndDocumento documento) throws IOException {
+
+        switch (documento.getDistribuidora()){
+            case FACUNDO: {
+                return facundoServicio.obtenerProductos(documento.getExcels());
+            }
+            case INDIAS: {
+                return indiasServicio.obtenerProductos(documento.getExcels());
+            }
+        }
+        return null;
+    }
+}
