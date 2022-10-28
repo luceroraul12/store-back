26171a34d77d2f2eb19a4a2a9ce39a29d1ab38e2
Lucero Raul <luceroraul12@gmail.com>
2022-10-28 17:10:23 -0300

    fix test

--- src/test/java/distribuidora/scrapping/services/webscrapping/LaGranjaDelCentroWebScrappingServicioTest.java
+++ src/test/java/distribuidora/scrapping/services/webscrapping/LaGranjaDelCentroWebScrappingServicioTest.java
@@ -1,5 +1,6 @@
 package distribuidora.scrapping.services.webscrapping;
 
+import distribuidora.scrapping.entities.productos.especificos.LaGranjaDelCentroEntidad;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -9,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -55,19 +58,28 @@ class LaGranjaDelCentroWebScrappingServicioTest {
     }
 
 
-//    @Test
-//    void obtenerProductosAPartirDeElements() throws IOException {
-//        Document docBueno = Jsoup.parse(new File("src/main/resources/static/la-granja-del-centro.html"));
-//        Elements elementosObtenidos = servicio.filtrarElementos(docBueno);
-//
-//        List<LaGranjaDelCentroEntidad> productos = servicio.(elementosObtenidos);
-//
-//        assertEquals("ZAPALLO EN ALMIBAR X 450 GRS", productos.get(0).getNombreProducto());
-//        assertEquals("ZARZAPARRILLA X 1 KG", productos.get(1).getNombreProducto());
-//        assertEquals("ZUCRA FORTE 9 X 200 CC", productos.get(2).getNombreProducto());
-//        assertEquals("ZUCRA FORTE 9 X 400 CC", productos.get(3).getNombreProducto());
-//
-//    }
+    @Test
+    void obtenerProductosAPartirDeElements() throws IOException {
+        Document docBueno = Jsoup.parse(new File("src/main/resources/static/la-granja-del-centro.html"));
+        Elements elementosObtenidos = servicio.filtrarElementos(docBueno);
+        List<LaGranjaDelCentroEntidad> productos = new ArrayList<>();
+        elementosObtenidos.forEach(elementProducto -> {
+            productos.add(
+                    servicio.obtenerProductosAPartirDeElements(elementProducto)
+            );
+        });
+
+        String d1 = productos.get(0).getNombreProducto();
+        String d2 = productos.get(1).getNombreProducto();
+        String d3 = productos.get(2).getNombreProducto();
+        String d4 = productos.get(3).getNombreProducto();
+
+        assertTrue("ZAPALLO EN ALMIBAR X 450 GRS".equalsIgnoreCase(d1));
+        assertTrue("ZARZAPARRILLA X 1 KG".equalsIgnoreCase(d2));
+        assertTrue("ZUCRA FORTE 9 X 200 CC".equalsIgnoreCase(d3));
+        assertTrue("ZUCRA FORTE 9 X 400 CC".equalsIgnoreCase(d4));
+
+    }
 
 
     /**
