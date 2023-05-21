e3ea01315e271ecd76bf3faeb9e10e2d5a1c2af1
homitowen <luceroraul12@gmail.com>
2023-05-21 17:51:14 -0300

    identando

--- src/main/java/distribuidora/scrapping/services/internal/InventorySystemImpl.java
+++ src/main/java/distribuidora/scrapping/services/internal/InventorySystemImpl.java
@@ -1,5 +1,6 @@
 package distribuidora.scrapping.services.internal;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -11,14 +12,21 @@ import distribuidora.scrapping.entities.ProductoInterno;
 import distribuidora.scrapping.enums.Distribuidora;
 import org.springframework.util.CollectionUtils;
 
-public class InventorySystemImpl implements InventorySystem {
+public class InventorySystemImpl
+		implements InventorySystem {
 
 	@Override
 	public int actualizarPreciosAutomatico() {
 		// llamado a las bases de datos para obtener los productos especificos e internos
 		List<ProductoInterno> productoInternos = new ArrayList<>();
 		List<ProductoEspecifico> productoEspecificos = new ArrayList<>();
+		// tengo en cuenta la fecha al comenzar el proceso
+		LocalDateTime now = LocalDateTime.now();
+
 		actualizarPrecioConProductosEspecificos(productoEspecificos, productoInternos);
+
+		// solo tengo en cuenta los productos que tienen fecha de modificacion por delante que la fecha en la que se
+		// inicia el actualizado
 
 
 		return 0;
@@ -28,25 +36,26 @@ public class InventorySystemImpl implements InventorySystem {
 	public void actualizarPrecioConProductosEspecificos(List<ProductoEspecifico> especificos,
 			List<ProductoInterno> internos) {
 		// agrupo por distribuidora / codigo de referencia tanto interno como especifico
-		Map<Distribuidora, Map<String, ProductoEspecifico>> mapEspecifico = especificos.stream()
-				.collect(
-						Collectors.groupingBy(e -> e.getDistribuidora(),
-						Collectors.toMap(e -> e.getId(), Function.identity())));
-		
-		Map<Distribuidora, Map<String, ProductoInterno>> mapInterno = internos.stream()
-				.collect(
-						Collectors.groupingBy(e -> e.getDistribuidoraReferencia(),
-						Collectors.toMap(e -> e.getCodigoReferencia(), Function.identity())));
-		
+		Map<Distribuidora, Map<String, ProductoEspecifico>> mapEspecifico = especificos.stream().collect(
+				Collectors.groupingBy(e -> e.getDistribuidora(),
+				                      Collectors.toMap(e -> e.getId(), Function.identity())));
+
+		Map<Distribuidora, Map<String, ProductoInterno>> mapInterno = internos.stream().collect(
+				Collectors.groupingBy(e -> e.getDistribuidoraReferencia(),
+				                      Collectors.toMap(e -> e.getCodigoReferencia(), Function.identity())));
+
 		// recorro los internos por que son los unicos que me interesan
 		for (Map.Entry<Distribuidora, Map<String, ProductoInterno>> mapInternoByDistribuidora : mapInterno.entrySet()) {
-			Map<String, ProductoEspecifico> matchDistribuidora = mapEspecifico.getOrDefault(mapInternoByDistribuidora.getKey(), null);
-			if (!CollectionUtils.isEmpty(matchDistribuidora)){
-				for (Map.Entry<String, ProductoInterno> mapInternoCodigoReferenciaProducto : mapInternoByDistribuidora.getValue().entrySet()) {
-					ProductoEspecifico matchProducto = matchDistribuidora.get(mapInternoCodigoReferenciaProducto.getKey());
-					if (matchProducto != null){
+			Map<String, ProductoEspecifico> matchDistribuidora = mapEspecifico.getOrDefault(
+					mapInternoByDistribuidora.getKey(), null);
+			if(! CollectionUtils.isEmpty(matchDistribuidora)) {
+				for (Map.Entry<String, ProductoInterno> mapInternoCodigoReferenciaProducto : mapInternoByDistribuidora.getValue()
+				                                                                                                      .entrySet()) {
+					ProductoEspecifico matchProducto = matchDistribuidora.get(
+							mapInternoCodigoReferenciaProducto.getKey());
+					if(matchProducto != null) {
 						Double precio = matchProducto.getPrecioExterno();
-						if (precio != null && precio > 0.0){
+						if(precio != null && precio > 0.0) {
 							mapInternoCodigoReferenciaProducto.getValue().setPrecio(precio);
 						}
 					}
