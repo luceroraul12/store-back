d841aab3daa20d70f4330d017b79ebbe15939493
Homitowen <luceroraul12@gmail.com>
2023-08-05 08:24:17 -0300

    Revert "Quito La granja del centro para no incluirla mas"
    This reverts commit 32c5dca28c157fdf1aa8d438377452938086a30a.

---
+++ src/main/java/distribuidora/scrapping/services/webscrapping/LaGranjaDelCentroWebScrappingServicio.java
@@ -0,0 +1,79 @@
+package distribuidora.scrapping.services.webscrapping;
+
+import distribuidora.scrapping.configs.Constantes;
+import distribuidora.scrapping.entities.productos.especificos.LaGranjaDelCentroEntidad;
+import distribuidora.scrapping.services.webscrappingconcurrent.WebScrappingConcurrent;
+import distribuidora.scrapping.util.LaGranjaDelCentroUtil;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class LaGranjaDelCentroWebScrappingServicio extends WebScrappingConcurrent<LaGranjaDelCentroEntidad> {
+
+    @Autowired
+    LaGranjaDelCentroUtil laGranjaDelCentroUtil;
+
+    /**
+     * Para este caso, contienen un apartado  de paginador.
+     * En el template aparece la siguiente etiqueta 'span' con los siguientes atributos:<br>
+     * span class="p-activo">123 <br>
+     * Esta etiqueta solo esta presente cuando es una pagina valida. Por lo tanto voy a condicionar en funcion a eso.
+     * @param document template de la pagina Web siguiente
+     * @return
+     */
+    @Override
+    protected boolean esDocumentValido(Document document) throws Exception {
+        boolean esValido = false;
+         for (Element element : document.getElementsByTag("span")){
+            if (element.hasClass("p-activo")){
+                esValido = true;
+            }
+         }
+
+        return esValido;
+    }
+
+    @Override
+    protected LaGranjaDelCentroEntidad obtenerProductosAPartirDeElements(Element elementProducto) {
+        return LaGranjaDelCentroEntidad.builder()
+                .distribuidoraCodigo(getDistribuidoraCodigo())
+                .nombreProducto(
+                        elementProducto.getElementsByClass("h3-content-1").text()
+                )
+                .precio(
+                        Double.valueOf(elementProducto
+                                .getElementsByClass("p-precio-content-1")
+                                .text()
+                                .replaceAll("[$.]","")
+                                .replaceAll(",","."))
+                )
+                .build();
+    }
+
+    @Override
+    protected Elements filtrarElementos(Document documento) {
+        return documento
+                .select("div.box-content-1");
+    }
+
+    @Override
+    protected void initImplementacion() {
+        setDistribuidoraCodigo(Constantes.LV_DISTRIBUIDORA_LA_GRANJA_DEL_CENTRO);
+        setEsBuscadorConPaginador(true);
+        setUrlBuscador("https://lagranjadelcentro.com.ar/productos.php?pagina=");
+    }
+
+    @Override
+    protected int generarUltimoIndicePaginador() throws IOException {
+        Document document = Jsoup.connect(getUrlBuscador()).get();
+        Elements element = document.select(".paginador > .p");
+        String url = element.get(1).attr("href");
+        return Integer.parseInt(url.split("=")[1]);
+    }
+}
---
+++ src/test/java/distribuidora/scrapping/services/webscrapping/LaGranjaDelCentroWebScrappingServicioTest.java
@@ -0,0 +1,92 @@
+package distribuidora.scrapping.services.webscrapping;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LaGranjaDelCentroWebScrappingServicioTest {
+
+    @Autowired
+    LaGranjaDelCentroWebScrappingServicio servicio;
+
+
+    @Test
+    void pruebaGeneral()  {
+
+    }
+
+    /**
+     * Prueba realizada 10/20/2022. Podria cambiar en el futuro.
+     * caso bueno < 123
+     * caso limite 123
+     * caso malo > 123
+     * @throws IOException
+     */
+    @Test
+    void esDocumentValido() throws IOException {
+        Document docBueno = Jsoup.connect("https://lagranjadelcentro.com.ar/productos.php?pagina=2").get();
+        Document docLimite = Jsoup.connect("https://lagranjadelcentro.com.ar/productos.php?pagina=125").get();
+        Document docMalo = Jsoup.connect("https://lagranjadelcentro.com.ar/productos.php?pagina=126").get();
+
+        try {
+            assertTrue(servicio.esDocumentValido(docBueno));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            assertTrue(servicio.esDocumentValido(docLimite));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            assertFalse(servicio.esDocumentValido(docMalo));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+//    @Test
+//    void obtenerProductosAPartirDeElements() throws IOException {
+//        Document docBueno = Jsoup.parse(new File("src/main/resources/static/la-granja-del-centro.html"));
+//        Elements elementosObtenidos = servicio.filtrarElementos(docBueno);
+//        List<LaGranjaDelCentroEntidad> productos = new ArrayList<>();
+//        elementosObtenidos.forEach(elementProducto -> {
+//            productos.add(
+//                    servicio.obtenerProductosAPartirDeElements(elementProducto)
+//            );
+//        });
+//
+//        String d1 = productos.get(0).getNombreProducto();
+//        String d2 = productos.get(1).getNombreProducto();
+//        String d3 = productos.get(2).getNombreProducto();
+//        String d4 = productos.get(3).getNombreProducto();
+//
+//        assertTrue("ZAPALLO EN ALMIBAR X 450 GRS".equalsIgnoreCase(d1));
+//        assertTrue("ZARZAPARRILLA X 1 KG".equalsIgnoreCase(d2));
+//        assertTrue("ZUCRA FORTE 9 X 200 CC".equalsIgnoreCase(d3));
+//        assertTrue("ZUCRA FORTE 9 X 400 CC".equalsIgnoreCase(d4));
+//
+//    }
+
+
+    /**
+     * Le paso un documento que siempre tiene objetos y deberia filtrar solo las partes que pueden contener elementos posibles de extraer informacion
+     */
+    @Test
+    void filtrarElementos() throws IOException {
+        Document docBueno = Jsoup.parse(new File("src/main/resources/static/la-granja-del-centro.html"));
+        Elements elementosObtenidos = servicio.filtrarElementos(docBueno);
+
+        assertEquals(4,elementosObtenidos.size());
+    }
+}
