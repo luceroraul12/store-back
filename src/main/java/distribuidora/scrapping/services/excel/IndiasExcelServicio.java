408243c1603d8f72b887cc7ab5a67b0017a40011
Homitowen <luceroraul12@gmail.com>
2023-09-02 15:40:37 -0300

    Indias: Se actualiza la forma de recuperar los rows ya que por fin han cambiado la estructura del excel

--- src/main/java/distribuidora/scrapping/services/excel/IndiasExcelServicio.java
+++ src/main/java/distribuidora/scrapping/services/excel/IndiasExcelServicio.java
@@ -2,6 +2,7 @@ package distribuidora.scrapping.services.excel;
 
 import distribuidora.scrapping.configs.Constantes;
 import distribuidora.scrapping.entities.productos.especificos.IndiasEntidad;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.springframework.stereotype.Service;
@@ -12,16 +13,25 @@ public class IndiasExcelServicio extends BusquedorPorExcel<IndiasEntidad> {
     @Override
     boolean esRowValido(Row row) {
         boolean resultado = false;
-        try{
-            if (row.getCell(1).getCellType().equals(CellType.STRING)){
-                if (row.getCell(2).getCellType().equals(CellType.NUMERIC)){
-                    if (row.getCell(3).getCellType().equals(CellType.STRING)){
-                        resultado = true;
-                    }
-                }
-            }
-        } catch (Exception ignored){
+        boolean rowGeneral = false;
+        boolean rowEspecifico = false;
+
+        try {
+            rowGeneral = row.getCell(1).getCellType().equals(CellType.STRING)
+                    && (row.getCell(3).getCellType().equals(CellType.STRING) || row.getCell(3).getCellType().equals(CellType.NUMERIC))
+                    && row.getCell(4).getCellType().equals(CellType.STRING)
+                    && row.getCell(6).getCellType().equals(CellType.NUMERIC);
+        } catch (Exception exception) {
         }
+
+        try {
+            rowEspecifico = row.getCell(1).getCellType().equals(CellType.STRING)
+                    && (row.getCell(2).getCellType().equals(CellType.STRING) || row.getCell(2).getCellType().equals(CellType.NUMERIC))
+                    && row.getCell(3).getCellType().equals(CellType.STRING)
+                    && row.getCell(4).getCellType().equals(CellType.NUMERIC);
+        } catch (Exception exception) {}
+;
+        resultado = rowGeneral || rowEspecifico;
         return resultado;
     }
 
--- src/main/java/distribuidora/scrapping/util/IndiasUtil.java
+++ src/main/java/distribuidora/scrapping/util/IndiasUtil.java
@@ -4,6 +4,7 @@ import distribuidora.scrapping.configs.Constantes;
 import distribuidora.scrapping.entities.Producto;
 import distribuidora.scrapping.entities.productos.especificos.IndiasEntidad;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.springframework.stereotype.Component;
 
@@ -28,14 +29,61 @@ public class IndiasUtil extends ProductoExcelUtil<IndiasEntidad>{
 
     @Override
     public IndiasEntidad convertirRowEnProductoEspecifico(Row row, String distribuidoraCodigo) {
-        double precio = validarPrecio(row.getCell(4));
-        return IndiasEntidad.builder()
-                .distribuidoraCodigo(distribuidoraCodigo)
-                .rubro(row.getCell(1).toString())
-                .codigo((int) row.getCell(2).getNumericCellValue())
-                .descripcion(row.getCell(3).toString())
-                .precio(precio)
-                .build();
+        boolean rowGeneral = false;
+        boolean rowEspecifico = false;
+        try {
+            rowGeneral = row.getCell(1).getCellType().equals(CellType.STRING)
+                    && (row.getCell(3).getCellType().equals(CellType.STRING) || row.getCell(3).getCellType().equals(CellType.NUMERIC))
+                    && row.getCell(4).getCellType().equals(CellType.STRING)
+                    && row.getCell(6).getCellType().equals(CellType.NUMERIC);
+        } catch (Exception exception) {
+        }
+
+        try {
+            rowEspecifico = row.getCell(1).getCellType().equals(CellType.STRING)
+                    && (row.getCell(2).getCellType().equals(CellType.STRING) || row.getCell(2).getCellType().equals(CellType.NUMERIC))
+                    && row.getCell(3).getCellType().equals(CellType.STRING)
+                    && row.getCell(4).getCellType().equals(CellType.NUMERIC);
+        } catch (Exception exception) {}
+
+        double precio;
+        Integer codigo;
+        if (rowGeneral){
+            precio = validarPrecio(row.getCell(6));
+            codigo = validarCodigo(row.getCell(3));
+            return IndiasEntidad.builder()
+                    .distribuidoraCodigo(distribuidoraCodigo)
+                    .rubro(row.getCell(1).toString())
+                    .codigo(codigo)
+                    .descripcion(row.getCell(4).toString())
+                    .precio(precio)
+                    .build();
+        } else if (rowEspecifico) {
+            precio = validarPrecio(row.getCell(4));
+            codigo = validarCodigo(row.getCell(2));
+            return IndiasEntidad.builder()
+                    .distribuidoraCodigo(distribuidoraCodigo)
+                    .rubro(row.getCell(1).toString())
+                    .codigo(codigo)
+                    .descripcion(row.getCell(3).toString())
+                    .precio(precio)
+                    .build();
+        }
+        return null;
+    }
+
+    private Integer validarCodigo(Cell cell) {
+        Integer codigo = 0;
+
+        try {
+            codigo = Integer.valueOf(cell.getStringCellValue());
+        } catch (Exception e){
+        }
+        try{
+            codigo = (int) cell.getNumericCellValue();
+        } catch (Exception e) {
+        }
+        return codigo;
     }
 
     private double validarPrecio(Cell cell) {
