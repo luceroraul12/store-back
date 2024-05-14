00c16bb5e9d8fd217b7509ccff9fd70e481d1367
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2024-05-14 08:18:40 -0300

    fix valores iniciales

--- src/main/resources/sql/1.34/data.sql
+++ src/main/resources/sql/1.34/data.sql
@@ -8,8 +8,8 @@ INSERT INTO public.datos_distribuidora
     web_url, 
     has_paginator)
 VALUES
-    (true, 1, '2024-05-12 19:45:27.174', 1403, true, false, 'http://gglobal.net.ar/bernal/?cliente', false),
+    (true, 1, '2024-05-12 19:45:27.174', 0, true, false, 'http://gglobal.net.ar/bernal/?cliente', false),
     (true, 3, '2024-05-12 19:45:27.174', 0, true, false, 'https://pidorapido.com/dongasparsj', false),
-    (true, 5, '2024-05-13 08:06:27.306', 2099, true, false, 'https://lagranjadelcentro.com.ar/productos.php?', true),
-    (true, 2, '2024-05-13 08:55:57.125', 1320, false, true, '', false),
+    (true, 5, '2024-05-13 08:06:27.306', 0, true, false, 'https://lagranjadelcentro.com.ar/productos.php?', true),
+    (true, 2, '2024-05-13 08:55:57.125', 0, false, true, '', false),
     (true, 6, NULL, 0, false, true, '', false);
