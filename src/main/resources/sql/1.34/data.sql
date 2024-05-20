6d6916a296dbb855ed6c286e60365ed95d722da1
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2024-05-19 22:58:19 -0300

    Fix sql y docker-compose de prueba antes de produccion

--- src/main/resources/sql/1.34/data.sql
+++ src/main/resources/sql/1.34/data.sql
@@ -10,7 +10,7 @@ INSERT INTO public.datos_distribuidora
 VALUES
     (true, 1, '2024-05-12 19:45:27.174', 0, true, false, 'http://gglobal.net.ar/bernal/?cliente', false),
     (true, 3, '2024-05-12 19:45:27.174', 0, true, false, 'https://pidorapido.com/dongasparsj', false),
-    (true, 5, '2024-05-13 08:06:27.306', 0, true, false, 'https://lagranjadelcentro.com.ar/productos.php?', true),
+    (true, 5, '2024-05-13 08:06:27.306', 0, true, false, 'https://lagranjadelcentro.com.ar/productos.php?pagina=', true),
     (true, 2, '2024-05-13 08:55:57.125', 0, false, true, '', false),
     (true, 6, NULL, 0, false, true, '', false);
 
@@ -19,12 +19,10 @@ VALUES
 update productos_internos 
 	set client_id = 1
 
+-- elimino los external producto que no fueron vinculados
+delete from external_product  
+where id not in (
+	select ep.id from external_product ep 
+		inner join  productos_internos p on p.external_product_id = ep.id
+);
 
-
--- Elimino las vinculaciones existentes VERRRRRRs
-update productos_internos p
-set external_product_id = null
-
--- Elimino todos los external_products para que los vuelva a buscar
-delete from external_product
--- TODO: VER SI CONVIENE ESTO O SI SE PUEDEN UNIFICAR TODOS LOS EXISTENTES PARA QUE PUEDAN VOLVER A SALIR
