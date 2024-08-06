a7cb43593f483ff8878e33c9afd513bc53ddbc38
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2024-08-06 07:24:45 -0300

    Fix Error handler to services

--- src/main/java/distribuidora/scrapping/security/handler/CustomException.java
+++ src/main/java/distribuidora/scrapping/security/handler/CustomException.java
@@ -2,15 +2,28 @@ package distribuidora.scrapping.security.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CustomException extends Throwable {
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<String> handleSecurityException(Exception ex) {
+
+	private static final long serialVersionUID = 1L;
+
+	@ExceptionHandler(UsernameNotFoundException.class) // Manejador específico												// primero
+	public ResponseEntity<ErrorResponse> handleUsernameNotFoundException(
+			UsernameNotFoundException ex) {
 		ex.printStackTrace();
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-				.body(ex.getMessage());
+
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+				.body(new ErrorResponse(ex.getMessage()));
+	}
+
+	@ExceptionHandler(Exception.class) // Manejador general después
+	public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
+		ex.printStackTrace();
+		return ResponseEntity.badRequest()
+				.body(new ErrorResponse(ex.getMessage()));
 	}
 }
---
+++ src/main/java/distribuidora/scrapping/security/handler/ErrorResponse.java
@@ -0,0 +1,13 @@
+package distribuidora.scrapping.security.handler;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@ResponseBody
+public class ErrorResponse {
+	private final String message;
+}
